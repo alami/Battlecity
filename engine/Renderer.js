48eadedab56cc7138eb3384b5ae4428edc7dc3bf
alami <alami@km.ru>
2019-11-09 16:31:58 +0500

    l2-0-45 Sprite, Container, del Readme показать во фрейме

--- engine/Renderer.js
+++ engine/Renderer.js
@@ -11,25 +11,30 @@
             this.canvas.height = args.height ||50
             this.update = args.update || (() => {})
 
+            this.stage = new GameEngine.Container
+
             requestAnimationFrame(timestamp => this.tick(timestamp))
         }
         tick (timestamp) {
-            this.clear()
             this.update(timestamp)
+
+            this.clear()
+            this.render()
             requestAnimationFrame(timestamp => this.tick(timestamp))
+        }
+        render () {
+            this.stage.draw(this.canvas,this.context)
         }
         draw (callback) {
             callback(this.canvas, this.context)
         }
         clear () {
-            this.draw((canvas, content) => {
-                content.fillStyle = this.background
-                content.beginPath()
-                content.rect(0, 0, canvas.width, canvas.height)
-                content.fill()
-            })
+            this.context.beginPath()
+            this.context.fillStyle = this.background
+            this.context.rect(0, 0, this.canvas.width, this.canvas.height)
+            this.context.fill()
+            }
         }
-    }
     window.GameEngine = window.GameEngine || {}
     window.GameEngine.Renderer = Renderer
 })();
---
+++ engine/templ.js
@@ -0,0 +1,10 @@
+;(function () {
+    'use strict'
+    class Container {
+        constructor () {
+
+        }
+    }
+    window.GameEngine = window.GameEngine || {}
+    window.GameEngine.Container = Container
+})();
