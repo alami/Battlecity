fa204777c1f84e21eee1da60cd5c66b0d73f55a3
alami <alami@km.ru>
2019-11-09 19:35:41 +0500

    l2-0-59-координаты фрейма и его положения во 2 аргументе

--- engine/Sprite.js
+++ engine/Sprite.js
@@ -2,15 +2,21 @@
     'use strict'
 
     class Sprite {
-        constructor (texture) {
+        constructor (texture, args = {}) {
             this.texture = texture
+
+            const frame = args.frame || {}
+
             this.frame = {
-                x:0, y:0, width: texture.width,height:texture.height
+                x: frame.x || 0,
+                y: frame.y || 0,
+                width:  frame.width  || texture.width,
+                height: frame.height || texture.height
             }
-            this.x=0
-            this.y=0
-            this.width=this.frame.width
-            this.height=this.frame.height
+            this.x = args.x || 0
+            this.y = args.y || 0
+            this.width = args.width || this.frame.width
+            this.height = args.height || this.frame.height
         }
         draw (canvas, context) {
             context.drawImage(
