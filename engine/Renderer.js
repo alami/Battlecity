d3977070a3d49bab927842b0a4ce443006ae0510
alami <alami@km.ru>
2019-11-09 13:37:57 +0500

    l2-0-29 clear() вращаем точку по кругу

--- engine/Renderer.js
+++ engine/Renderer.js
@@ -6,18 +6,28 @@
             this.canvas = document.createElement('canvas')
             this.context = this.canvas.getContext('2d')
 
+            this.background = args.background || 'black'
             this.canvas.width = args.width || 50
             this.canvas.height = args.height ||50
             this.update = args.update || (() => {})
 
-            requestAnimationFrame(timestamp => this.tick())
+            requestAnimationFrame(timestamp => this.tick(timestamp))
         }
         tick (timestamp) {
+            this.clear()
             this.update(timestamp)
-            requestAnimationFrame(timestamp => this.tick())
+            requestAnimationFrame(timestamp => this.tick(timestamp))
         }
         draw (callback) {
             callback(this.canvas, this.context)
+        }
+        clear () {
+            this.draw((canvas, content) => {
+                content.fillStyle = this.background
+                content.beginPath()
+                content.rect(0, 0, canvas.width, canvas.height)
+                content.fill()
+            })
         }
     }
     window.GameEngine = window.GameEngine || {}
