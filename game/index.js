85eb8023eef2d6519bb515bc816e0f4061d54d91
alami <alami@km.ru>
2019-12-13 16:14:00 +0500

    ch4-1-09 выделить index.js

---
+++ game/index.js
@@ -0,0 +1,31 @@
+const { Game, Scene, Sprite } = GameEngine
+
+const mainScene = new Scene ({
+    autoStart: true,
+    loading (loader) {
+        loader.addImage('bunny', 'static/bunny.jpg')
+        loader.addJson('persons', 'static/persons.json')
+    },
+    init () {
+        const bunnyTexture = this.parent.loader.getImage('bunny')
+        this.sprite = new Sprite(bunnyTexture, {
+            scale: 0.25,
+            anchorX: 0.5,
+            anchorY: 0.5,
+            x: this.parent.renderer.canvas.width / 2,
+            y: this.parent.renderer.canvas.height /2,
+        })
+        this.add(this.sprite)
+    },
+    update (timestamp) {
+        this.sprite.rotation = timestamp / 1000
+    }
+})
+
+const game = new Game ({
+    el: document.body,
+    width: 500,
+    height: 500,
+    background: 'green',
+    scenes: [mainScene]
+})
