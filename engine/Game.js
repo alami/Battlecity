3b312b54d2500d89dd04ee78dbe2afe3ffe7d1ea
alami <alami@km.ru>
2019-12-13 13:54:10 +0500

    ch3-0-50 Game-бизнес класс управлющий остальными

--- engine/Game.js
+++ engine/Game.js
@@ -14,11 +14,16 @@
             }
             const autoStartedScenes = this.scenes.filter(x => x.autoStart)
             for (const scene of autoStartedScenes) {
+                scene.status = 'loading'
                     scene.loading(this.loader)
             }
             this.loader.load(() => {
                 for (const scene of autoStartedScenes) {
+                    scene.status = 'init'
                     scene.init()
+                }
+                for (const scene of autoStartedScenes) {
+                    scene.status = 'started'
                 }
             })
             requestAnimationFrame(timestamp => this.tick(timestamp))
@@ -35,9 +40,14 @@
         }
         tick (timestamp) {
             //this.update(timestamp)
-            this.renderer.clear()
             //this.renderer.render()
+            for (const scene of this.scenes) {
+                if (scene.status === 'started') {
+                    scene.update(timestamp)
+                }
+            }
 
+           this.renderer.clear()
             requestAnimationFrame(timestamp => this.tick(timestamp))
         }
     }
